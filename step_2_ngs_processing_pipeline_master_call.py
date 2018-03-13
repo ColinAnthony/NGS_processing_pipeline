bf6d4ef27b4795ca8b7948314bdbcc2b1d49523e
colin <colin.anthony001@gmail.com>
2018-03-13 16:55:54 +0200

    added todo for possible bug to fix

--- step_2_ngs_processing_pipeline_master_call.py
+++ step_2_ngs_processing_pipeline_master_call.py
@@ -193,6 +193,9 @@ def call_align(envelope, script_folder, to_align, aln_path, fname):
 
 def main(path, name, gene_region, fwd_primer, cDNA_primer, nonoverlap, frame, stops, length, envelope, run_step,
          run_only):
+    #Todo: there is probably a bug when specifying -rs, where -rs is not 1 or 2
+    #Todo: need to check whether the data from 0new_data gets to the correct temp_folder
+
     get_script_path = os.path.realpath(__file__)
     script_folder = os.path.split(get_script_path)[0]
     path = os.path.abspath(path)
