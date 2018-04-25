99283ae3dbf2c93c08f116dc8ab1a7a6ab136506
colin.anthony001@gmail.com <colin.anthony001@gmail.com>
2018-04-25 16:14:37 +0200

    fixed the alignment call removed unnecessary reading in and writing out of files

--- align_all_samples.py
+++ align_all_samples.py
@@ -2,7 +2,6 @@
 from __future__ import print_function
 from __future__ import division
 import os
-import tempfile
 import subprocess
 from subprocess import DEVNULL
 import argparse
@@ -46,63 +45,25 @@ def fasta_to_dct_rev(file_name):
     return dct
 
 
-def align_dna(dna_dict, tmp_file):
+def align_dna(in_file, out_file):
     """
     does a mafft multiple sequence alignment on a fasta file
-    :param dna_dict (dict) dictionary containing the conserved regions f the sequence
-    :param tmp_file: (srt) path to temp folder
+    :param in_file: (str) path and filename of the file to align
+    :param out_file: (str) path and nae of the aligned outfile
     :return: (dict) dictionary of aligned sequences for all conserved regions
     """
-    # write temp outfile
-    tmp_file_in = os.path.join(tmp_file + '.fasta')
-    tmp_file_out = os.path.join(tmp_file + '.aln')
-    for seq, name_list in dna_dict.items():
-        with open(tmp_file_in, 'a') as handle1:
-            handle1.write('>' + str(name_list[0]) + '\n' + str(seq) + '\n')
-
-    cmd = 'mafft {0} > {1}'.format(tmp_file_in, tmp_file_out)
+    cmd = 'mafft {0} > {1}'.format(in_file, out_file)
+    print("Aligning the sequences, please wait")
     subprocess.call(cmd, shell=True, stdout=DEVNULL, stderr=DEVNULL)
     # todo error message add mafft to path
-    # print("Aligning the sequences, please wait")
-
-    # import the aligned file
-    aln_seqs = fasta_to_dct_rev(tmp_file_out)
-
-    # remove the temp files
-    os.remove(tmp_file_in)
-    os.remove(tmp_file_out)
-
-    new_aln_seqs = collections.defaultdict()
-    for seq in aln_seqs.keys():
-        lookup_sequence = seq.replace("-", "")
-        names_list = dna_dict[lookup_sequence]
-        new_aln_seqs[seq] = names_list
-
-    return new_aln_seqs
 
 
 def main(infile, outpath, name):
-
+    outpath = os.path.abspath(outpath)
+    infile = os.path.abspath(infile)
     outfile = os.path.join(outpath, name)
-    tmp_path = tempfile.gettempdir()
-    tmp_file = os.path.join(tmp_path, name)
-
-    # import sequences into dictionary
-    d = fasta_to_dct_rev(infile)
-
     # run mafft alignment on sequences
-    align_d = align_dna(d, tmp_file)
-
-    # overwrite existing file if present
-    with open(outfile, 'w') as handle:
-        handle.write('')
-
-    # write aligned sequences to file
-    with open(outfile, 'w') as handle:
-        for seq, name_list in align_d.items():
-            for seq_name in name_list:
-                out_name = '>' + seq_name + '\n'
-                handle.write(out_name + seq + '\n')
+    align_dna(infile, outfile)
 
     print("Aligning completed")
 
